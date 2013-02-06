a32396ec631e709ab6873dbdfcc70d75d371bc14
Malik Alayli <malayli@viadeoteam.com>
2013-02-06 11:36:26 +0100

    New files

--- Viadeo-iOS-SDK/Samples/iPhone/build/MySuperApplication.build/Debug-iphonesimulator/MySuperApplication.build/Objects-normal/i386/VDPopUp.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/viaduc357/Documents/iOS/api-sdk/trunk/samples/iPhone/MySuperApplication/../../../src/Viadeo-iOS-SDK/Main/VDPopUp.m \
-  /Users/viaduc357/Documents/iOS/api-sdk/trunk/samples/iPhone/MySuperApplication/../../../src/Viadeo-iOS-SDK/Main/VDPopUp.h \
-  /Users/viaduc357/Documents/iOS/api-sdk/trunk/samples/iPhone/MySuperApplication/../../../src/Viadeo-iOS-SDK/Main/VDConstants.h
--- Viadeo-iOS-SDK/Samples/iPhone/build/MySuperApplication.build/Debug-iphonesimulator/MySuperApplication.build/Objects-normal/i386/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/viaduc357/Documents/iOS/api-sdk/trunk/samples/iPhone/MySuperApplication/main.m
